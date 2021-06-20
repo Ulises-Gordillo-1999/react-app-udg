6eb487fc661e51e5217054233967516282617018
Ulises-Gordillo-1999 <uligordillo@gmail.com>
2021-06-20 13:56:20 -0300

    ItemDetailContainer, ModalDetail, FetchPokeApi

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,25 +18,5 @@ export default function NavBar() {
         <CartWidget/>
       </Form>
     </Navbar>
-    /*<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">UDG Ventas</Navbar.Brand>
-        <CartWidget/>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>*/
   );
 }
