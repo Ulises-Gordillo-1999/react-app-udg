1c85dd76c88e44789e7a0850b886773c851884f1
Ulises-Gordillo-1999 <uligordillo@gmail.com>
2021-07-07 03:03:03 -0300

    Firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import FetchPokeApi from "./components/FetchPokeApi";
 import AppRouter from "./routers/AppRouter";
